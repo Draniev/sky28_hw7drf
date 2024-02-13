6b513e30e225147b18396b537c50398e5fb94299
Sergei Popov <sergei.v.popov@icloud.com>
2024-02-13 19:02:14 +0300

    fix youtube validator

--- materials/validators.py
+++ materials/validators.py
@@ -9,7 +9,8 @@ class CheckVideoUrlValidator:
 
     def __call__(self, value):
         video_url = dict(value).get(self.field)
-        x = re.search(r"^https://youtube.com/[A-Za-z0-9\=\?]+", video_url)
-        if not x:
-            raise ValidationError("Надо загружать с Ютуба!")
+        if video_url:
+            x = re.search(r"^https://youtube.com/[A-Za-z0-9\=\?]+", video_url)
+            if not x:
+                raise ValidationError("Надо загружать с Ютуба!")
 
