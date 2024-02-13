27414ad0d813f16316552cfe83cb2a559a927cca
Sergei Popov <sergei.v.popov@icloud.com>
2024-02-13 11:17:17 +0300

    hw 25.2 task 1

--- materials/serializers.py
+++ materials/serializers.py
@@ -2,12 +2,14 @@ from rest_framework import serializers
 from rest_framework.fields import SerializerMethodField
 
 from .models import Course, Lesson
+from .validators import CheckVideoUrlValidator
 
 
 class LessonSerializer(serializers.ModelSerializer):
     class Meta:
         model = Lesson
         fields = '__all__'
+        validators = [CheckVideoUrlValidator(field='video'), ]
 
 
 class CourseSerializer(serializers.ModelSerializer):
---
+++ materials/validators.py
@@ -0,0 +1,15 @@
+import re
+
+from rest_framework.serializers import ValidationError
+
+
+class CheckVideoUrlValidator:
+    def __init__(self, field):
+        self.field = field
+
+    def __call__(self, value):
+        video_url = dict(value).get(self.field)
+        x = re.search(r"^https://youtube.com/[A-Za-z0-9\=\?]+", video_url)
+        if not x:
+            raise ValidationError("Надо загружать с Ютуба!")
+
